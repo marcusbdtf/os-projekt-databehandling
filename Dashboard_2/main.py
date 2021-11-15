8a2dd1d2db7ed11f4e2563576212e8e6aeb93272
marcusbdtf <89519696+marcusbdtf@users.noreply.github.com>
2021-11-15 14:53:28 +0100

    update to dashboard, also added data processing function

--- Dashboard_2/main.py
+++ Dashboard_2/main.py
@@ -4,6 +4,8 @@ import dash
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import hashlib as hl
+import plotly_express as px
+from dash.dependencies import Input, Output
 pd.options.plotting.backend = "plotly"
 
 """Data"""
@@ -17,37 +19,35 @@ italy_df = italy_df[italy_df["Team"]=="Italy"]
 italy_most_medals = italy_df.groupby("Sport").count().reset_index()
 italy_most_medals = italy_most_medals.sort_values(by="Medal",ascending=False)
 
-italy_city = italy_df.groupby("City").count().reset_index()
-italy_city = italy_city.sort_values(by="Medal", ascending = False)
+def process_data(old_df, col):
+    filtered_df = old_df.groupby(col).count().reset_index()
+    filtered_df = filtered_df.sort_values(by=col, ascending=False)
+    return filtered_df 
 
-fig = make_subplots(1,3)
-fig.add_trace(
-    go.Bar(
-        x=italy_most_medals["Sport"], 
-        y=italy_most_medals["Medal"]
-        )
-    )
-fig.add_trace(
-    go.Bar(
-        x=italy_most_medals[0:10]["Sport"], 
-        y=italy_most_medals[0:10]["Medal"]),
-    row=1,col=2)
+app = dash.Dash()
 
-fig.add_trace(
-    go.Bar(
-        x=italy_city['City'],
-        y=italy_city['Medal']),
-        row=1,col=3)
-
-dropdown_dict = dict(Sport="Medal", )
-
-app = dash.Dash(__name__)
-
-app.layout = html.Div([
-    html.H1("Olympics Data"),
-    
-    dcc.Graph(id='os-graph', figure=fig),
+app.layout = html.Div(children=[
+    html.H1(children='Italy Olympics Graphs'),
+    dcc.Dropdown(id='olympics-dropdown',
+                    options=[{'label': i, 'value': i}
+                    for i in italy_df],
+                    value='Sport'),
+    dcc.Graph(id='medals-graph')
 ])
+
+@app.callback(
+    Output(component_id='medals-graph', component_property='figure'),
+    Input(component_id='olympics-dropdown', component_property='value')
+)
+
+def update_graph(selected_option):
+    filtered_italy = process_data(italy_df, selected_option)
+    fig = px.bar(filtered_italy,
+                    x = selected_option, y = 'Medal',
+                    color='Sport',
+                    title=f'Gold Medals in {selected_option}')
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
