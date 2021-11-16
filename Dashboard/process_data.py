8472c96330f7a9c577a844c8bf5a38396440e7dd
Lukas Källström AIU21 <89090277+LukasKallstrom@users.noreply.github.com>
2021-11-16 16:57:50 +0100

    added get all countries function and funtionality

--- Dashboard/process_data.py
+++ Dashboard/process_data.py
@@ -5,12 +5,20 @@ athletes = pd.read_csv("Data/athlete_events.csv")
 hashed_names = athletes["Name"].apply(lambda x: hl.sha256(x.encode()).hexdigest())
 athletes.insert(1,"Hashed name", hashed_names)
 athletes = athletes.drop(columns="Name")
-italy_df = athletes.drop_duplicates(subset=["Medal", "Games","Event"])
-italy_df = italy_df[italy_df["NOC"]=="ITA"]
+athletes = athletes.drop_duplicates(subset=["Medal", "Games","Event"])
+all_countries_df = athletes
+all_countries_df["Total"] = all_countries_df["ID"]
+all_countries_df.drop(axis=1, columns=["ID", "Hashed name", "Team"])
+
+italy_df = athletes[athletes["NOC"]=="ITA"]
 italy_df["Total"] = italy_df["ID"]
+italy_df.drop(axis=1,columns=["ID", "Hashed name", "Team", "NOC"])
 
 def get_italy_data():
-    return italy_df.drop(axis=1,columns=["ID", "Hashed name", "Team", "NOC"])
+    return italy_df
+
+def get_all_countries():
+    return all_countries_df
 
 def process_data(old_df, col):
     filtered_df = old_df.groupby(col).count().reset_index()
