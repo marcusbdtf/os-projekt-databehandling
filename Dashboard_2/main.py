d66d83733fdb01bd97b34a957a41985ad5a22842
marcusbdtf <89519696+marcusbdtf@users.noreply.github.com>
2021-11-16 14:58:53 +0100

    added navbar, have to connect second DF and fix overlapping issues

--- Dashboard_2/main.py
+++ Dashboard_2/main.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 import hashlib as hl
 import plotly_express as px
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 pd.options.plotting.backend = "plotly"
 
 """Data"""
@@ -24,22 +25,85 @@ def process_data(old_df, col):
     filtered_df = filtered_df.sort_values(by=col, ascending=False)
     return filtered_df 
 
-app = dash.Dash()
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = html.Div(children=[
-    html.H1(children='Italy Olympics Graphs'),
+content_layout = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar_layout = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "4rem 1rem 2rem",
+    "background-color": "#f8f9fa",
+}
+
+
+sidebar = html.Div([
+    html.H2("Sidebar", className="display-4"),
+    html.Hr(),
+    html.P(
+        "Historic olympic performances"
+    ),
+        dbc.Nav([
+         dbc.NavLink("Italy Statistics", href="/", active="exact"),
+            dbc.NavLink("Sports statistics", href="/page-2", active="exact"),
+          ],
+          vertical=True,
+          pills=True,
+        ),
+    ],
+    style=sidebar_layout,
+)
+
+content = html.Div(id="page-content", children=[], style=content_layout)
+
+app.layout = html.Div([
+    sidebar,
+    content,
+    dcc.Location(id="url"),
+    html.H1('Italy Olympics Graphs'),
     dcc.Dropdown(id='olympics-dropdown',
                     options=[{'label': i, 'value': i}
                     for i in italy_df],
-                    value='Sport'),
-    dcc.Graph(id='medals-graph')
-])
+                    value='Sport',
+                    style={
+                            "width": "50%",
+                        },
+                    ),
+    dcc.Graph(id='medals-graph',
+              style={
+                    'height': 500,
+                    'width': 900,
+                    },
+                ),
+    ], style = {'margin':'auto','width': "50%"}
+    #style = {"display": "flex"}
+)
+
+@app.callback(
+    Output("page-content", "children"),
+    [Input("url", "pathname")],
+    Input(component_id='olympics-dropdown', component_property='value')
+    
+    
+)
+def page_loader(pathname, selected_option):
+    if pathname == "/":
+        return update_graph(selected_option)
+
+    elif pathname == "page-2":
+        pass
 
 @app.callback(
     Output(component_id='medals-graph', component_property='figure'),
-    Input(component_id='olympics-dropdown', component_property='value')
+    Input(component_id='olympics-dropdown', component_property='value'),
 )
-
 def update_graph(selected_option):
     filtered_italy = process_data(italy_df, selected_option)
 
