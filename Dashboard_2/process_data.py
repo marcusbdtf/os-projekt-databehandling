49c02dd8aa2f9b04ea03d07acabe6f0a1951548a
marcusbdtf <89519696+marcusbdtf@users.noreply.github.com>
2021-11-17 13:51:15 +0100

    added process data dashboard 2

---
+++ Dashboard_2/process_data.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import hashlib as hl
+"""Data"""
+athletes = pd.read_csv("Data/athlete_events.csv")
+hashed_names = athletes["Name"].apply(lambda x: hl.sha256(x.encode()).hexdigest())
+athletes.insert(1,"Hashed name", hashed_names)
+athletes = athletes.drop(columns="Name")
+athletes["Total"] = athletes["ID"]
+athletes = athletes.drop_duplicates(subset=["Medal", "Games","Event"])
+athletes.drop(axis=1, columns=["ID", "Hashed name", "Team"], inplace=True)
+
+all_countries_df = athletes.reset_index()
+italy_df = athletes[athletes["NOC"]=="ITA"].drop(axis=1, columns="NOC").reset_index()
+
+def get_italy_data():
+    return italy_df
+
+def get_all_countries():
+    return all_countries_df
+
+def process_data(old_df, col):
+    filtered_df = old_df.groupby(col).count().reset_index()
+    filtered_df = filtered_df.sort_values(by="Total", ascending=False)
+    filtered_df = filtered_df[filtered_df[col].notna()]
+    filtered_df = filtered_df[[col, "Total"]]
+    return filtered_df
